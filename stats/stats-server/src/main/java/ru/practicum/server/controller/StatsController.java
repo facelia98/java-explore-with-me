89d748c3a68ecbd537082329d46d580b6c8c5ccf
facelia98 <facelia98@mail.ru>
2023-07-20 11:11:06 +0300

    fix mistakes

--- stats/stats-server/src/main/java/ru/practicum/server/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/server/controller/StatsController.java
@@ -2,27 +2,33 @@ package ru.practicum.server.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.server.service.StatsService;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/stats")
+@RequestMapping
 @RequiredArgsConstructor
 public class StatsController {
     private final StatsService statsService;
 
-    @GetMapping
+    @GetMapping("/stats")
     public List<ViewStatsDto> getStats(@RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
                                        @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                        @RequestParam(name = "unique", defaultValue = "false") Boolean unique,
                                        @RequestParam(name = "uris", required = false) List<String> uris) {
         return statsService.getStats(start, end, unique, uris);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/hit")
+    public void createHit(@RequestBody @Valid EndpointHitDto endpointHitDto) {
+        statsService.create(endpointHitDto);
     }
 }
